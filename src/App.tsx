2eefbf802e7a5565d63ce0e610d603ac6b4fbc5c
Sam Rho <seungnam2@gmail.com>
2020-02-16 21:32:20 +0900

    cleanup: remove all files and folders that no longer used
    those files include all sorts of components that I had made
    for experimental purpose

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,7 @@ import "./App.css";
 import Counter from "./components/Counter/Counter";
 
 const App: React.FC = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<Counter />
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+	return <Counter />;
 };
 
 export default App;
